56748cc0972250737523e167a5c63f50eaaafb14
kimngan1011 <kimngan.doan@manabie.com>
2024-11-23 15:38:28 +0700

    create-one-time-lesson

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
 
-  timeout: 180000,
+  timeout: 300000,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -33,6 +33,7 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     video: 'on',
+    screenshot: 'only-on-failure',
   },
 
   /* Configure projects for major browsers */
--- tests/scripts/create-lesson.spec.ts
+++ tests/scripts/create-lesson.spec.ts
@@ -4,18 +4,30 @@ import { LsCommonTest } from "../pages/common-test";
 import { loginBO } from "../sf-login-page";
 import { CreateLessonAllocation } from "../pages/create-lesson-allocation";
 import { LESSON_URL } from "../utils/url";
+import { LESSON_NAME } from "../utils/masterData";
+import { BOLesson } from "../pages/bo-lesson";
 
-test("create-one-time-individual-lessons", async ({ page }) => {
+test("create-one-time-individual-lessons-with-student-teacher", async ({ page }) => {
     const createLessonAllocation = new CreateLessonAllocation(page);
     const lessonAllocationName = await createLessonAllocation.createLessonAllocation();
     const createLesson = new CreateLesson(page);
     const individualLessonName = await createLesson.createOneTimeIndividualLesson();
     const lsCommonTest = new LsCommonTest(page);
-    await lsCommonTest.searchList(individualLessonName);
-    await createLesson.addStudent(lessonAllocationName);
-    await lsCommonTest.redirectToTab('Report');
+    const boLesson = new BOLesson(page);
+    await lsCommonTest.searchList(individualLessonName); // open lesson detail
+    await createLesson.addTeacher(LESSON_NAME.teacher); // add student
+    await createLesson.addStudent(lessonAllocationName); // add teacher
+    await lsCommonTest.redirectToTab('Report'); // check report info
     await createLesson.checkLessonReportInfo('Teaching MethodIndividual');
     await createLesson.checkLessonReportInfo('Lesson Report StatusDraft');
+    await lsCommonTest.redirectToTab('Participants'); // check LA info
+    await createLesson.checkStudentSession(lessonAllocationName);
+    await loginBO(page, 'full'); // login BO
+    await boLesson.searchStudent(lessonAllocationName); // check lesson info on BO
+    await boLesson.openLessonDetail();
+    await page.waitForTimeout(5000);
+    await lsCommonTest.redirectToTab ('Student');
+    await lsCommonTest.redirectToTab ('Report');
 });
 
 

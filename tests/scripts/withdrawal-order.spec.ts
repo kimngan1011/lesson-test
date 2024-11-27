5b6ed3e52c44e026bac9717cb4b193f3f529d769
Manabie <manabie@Manabies-MacBook-Pro.local>
2024-11-27 22:07:07 +0700

    Submit and cancel withdrawal order

--- tests/scripts/withdrawal-order.spec.ts
+++ tests/scripts/withdrawal-order.spec.ts
@@ -8,9 +8,49 @@ test('Submit withdrawal order with start date < last attendance day', async ({ p
     const createLessonAllocation = new CreateLessonAllocation(page);
     const lessonAllocationName = await createLessonAllocation.createLessonAllocation(); // create LA
 
-    await withdrawalOrder.withdrawalOrder();
+    await withdrawalOrder.withdrawalOrder('UpdateLA');
     await lsCommonTest.openHyperlink(lessonAllocationName);
     await lsCommonTest.redirectToTab('Course');
-    await withdrawalOrder.checkLADurationAndPS('Purchased Slot5/Week');
+    await withdrawalOrder.checkLADurationAndPS('Purchased Slot5/Week', 'Submit');
 })
+
+test('Submit withdrawal order with start date = last attendance day', async ({ page }) => {
+    const withdrawalOrder = new CreateLessonAllocation (page);
+    const lsCommonTest = new LsCommonTest(page);
+    const createLessonAllocation = new CreateLessonAllocation(page);
+    const lessonAllocationName = await createLessonAllocation.createLessonAllocation(); // create LA
+
+    await withdrawalOrder.withdrawalOrder('DeleteLA');
+    await lsCommonTest.openHyperlink(lessonAllocationName);
+    await lsCommonTest.redirectToTab('Course');
+    await withdrawalOrder.checkLAInfo('Lesson Allocation (0)');
+})
+
+test('Cancel withdrawal order with start date < last attendance day', async ({ page }) => {
+    const withdrawalOrder = new CreateLessonAllocation (page);
+    const lsCommonTest = new LsCommonTest(page);
+    const createLessonAllocation = new CreateLessonAllocation(page);
+    const lessonAllocationName = await createLessonAllocation.createLessonAllocation(); // create LA
+
+    await withdrawalOrder.withdrawalOrder('UpdateLA');
+    await withdrawalOrder.cancelWithdrawal();
+    await lsCommonTest.openHyperlink(lessonAllocationName);
+    await lsCommonTest.redirectToTab('Course');
+    await withdrawalOrder.checkLADurationAndPS('Purchased Slot5/Week', 'Cancel');
+})
+
+test('Cancel withdrawal order with start date = last attendance day', async ({ page }) => {
+    const withdrawalOrder = new CreateLessonAllocation (page);
+    const lsCommonTest = new LsCommonTest(page);
+    const createLessonAllocation = new CreateLessonAllocation(page);
+    const lessonAllocationName = await createLessonAllocation.createLessonAllocation(); // create LA
+
+    await withdrawalOrder.withdrawalOrder('DeleteLA');
+    await withdrawalOrder.cancelWithdrawal();
+    await lsCommonTest.openHyperlink(lessonAllocationName);
+    await lsCommonTest.redirectToTab('Course');
+    await withdrawalOrder.checkLADurationAndPS('Purchased Slot5/Week', 'Cancel');
+})
+
+
 
